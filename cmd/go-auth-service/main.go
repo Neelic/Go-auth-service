a37152a7cffcfbc64d862300e23586cf674787fc
Neelic <salnikov.danila@mail.ru>
2024-10-04 13:17:22 +0300

    Добавлен логгер

--- cmd/go-auth-service/main.go
+++ cmd/go-auth-service/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "url-service/internal/config"
+import (
+	"log/slog"
+	"os"
+	"url-service/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	cfg := config.NewConfig()
+
+	log := setupLogger(cfg.Env)
+	log.Info("Starting service", "env", cfg.Env)
+
+	
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
